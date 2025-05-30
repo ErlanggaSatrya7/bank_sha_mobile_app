4ce691ca86c4ca678ced5b4278e3ae31fc706673
4n994 <anggasatrya007@gmail.com>
2025-05-30 16:57:12 +0700

    feat: add user profil in homepages

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -57,11 +57,70 @@ class HomePage extends StatelessWidget {
         onPressed: () {},
         backgroundColor: purpleColor,
         shape: const CircleBorder(),
-        child: Image.asset('assets/ic_plus_circle.png',
-        width: 24,),
+        child: Image.asset('assets/ic_plus_circle.png', width: 24),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [buildProfile()],
+      ),
+    );
+  }
+
+  Widget buildProfile() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Angga', style: greyTextStyle.copyWith(fontSize: 16)),
+              const SizedBox(height: 2),
+              Text(
+                'Satrya',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                width: 60,
+                height: 60,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/img_profile.png'),
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: whiteColor,
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.check_circle,
+                        color: greenColor,
+                        size: 14,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
